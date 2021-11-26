1dbae5b0b6c0ee353e92419e2d2feb72b8bab556
J. Antonio Guzmán Q <antguz06@gmail.com>
2021-11-26 12:37:27 -0600

    A linear model to calculate the slope that represent trend

---
+++ R/fun_slope.R
@@ -0,0 +1,19 @@
+################################################################################
+##### A linear model to calculate the slope that represent trend
+################################################################################
+
+#-------------------------------------------------------------------------------
+#Arguments
+
+#y: value information
+#Date: an integer that describe the julian day
+
+#-------------------------------------------------------------------------------
+#Functions
+fun_slope <- function(y, Date) { 
+  if(all(is.na(y))) {
+    NA
+  } else {
+    m = lm(y ~ Date); summary(m)$coefficients[2] 
+  }
+}
