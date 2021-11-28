6b157b9f7dde59717dad239562b472e1a9303e36
J. Antonio Guzmán Q <antguz06@gmail.com>
2021-11-28 16:24:37 -0600

    Get trends

--- R/trends_sen2.R
+++ R/trends_sen2.R
@@ -7,7 +7,7 @@
 
 source("R/path_scenes.R")
 source("R/get_mask.R")
-source("R/get_trend.R")
+source("R/fun_slope.R")
 
 #-------------------------------------------------------------------------------
 # Libraries
@@ -19,23 +19,41 @@ library(terra)
 #Arguments
 
 #root_path: select the root path of where the scenes are located
-#mask_months: period in which mask will be estimated for
-#evaluation_months: period in which trends will be estimated
-#threads: the number of threads to use for parallel processing
 root_path <- "/media/antonio/Work/Oak-Sentinel/level3_sen2"
-mask_doy <- c(152, 181) #The month of may
-evaluation_doy <- c(152, 243) #May to August
+
+#mask_months: period in which mask will be estimated for
+mask_doy <- c(152, 212) #The month of june
+
+#evaluation_months: period in which trends will be estimated
+evaluation_doy <- c(166, 258) #May to August
+
+#out_path: path of the outputs
+out_path <- "/media/antonio/Work/Oak-Sentinel/level4_sen2"
+
+#threads: the number of threads to use for parallel processing
+threads <- 28
+
 
 #-------------------------------------------------------------------------------
 #Function
-
-trends_sen2 <- function(root_path, mask_doy, evaluation_doy, threads) {
+trends_sen2 <- function(root_path, mask_doy, evaluation_doy, out_path, threads) {
   
   #Get scenes to work with
   frame <- path_scenes(root_path)
   
   #Unique tiles
   unique_tile <- unique(frame$tile)
+  
+  #Unique years
+  unique_years <- unique(year(frame$date))
+  
+  #Day of the year
+  frame$doy <- yday(frame$date)
+  
+  #Progress bar
+  pb <- txtProgressBar(min = 1, 
+                       max = length(unique_tile)*length(unique_years), 
+                       style = 3)
   
   # i <- 132
   #Loop over tiles
@@ -44,16 +62,17 @@ trends_sen2 <- function(root_path, mask_doy, evaluation_doy, threads) {
     #Subset to work with a given tile
     sub_tile <- subset(frame, tile == unique_tile[i])
     
-    #Unique years
-    unique_years <- unique(year(sub_tile$date))
-    
     #ii <- 6
     #Loop over years
-    for(ii in 1:length(sub_tile)) {
+    for(ii in 6) { #1:length(sub_tile)
+      
+      #Progress
+      setTxtProgressBar(pb, i*ii)
       
       #Subset layers for a given year
       sub_year <- subset(sub_tile, year(date) == unique_years[ii])
-      sub_year$doy <- yday(sub_year$date)
+      
+      if(nrow(sub_year) >= 10) {
       
       ###Mask-------------------------------------------------------------------
       #Mask list
@@ -62,7 +81,16 @@ trends_sen2 <- function(root_path, mask_doy, evaluation_doy, threads) {
                             mask_scenes$tile, "/", mask_scenes$scene)
       
       #Mask
-      mask_layer <- get_mask(mask_layer, threshold = 0.5)
+      mask_layer <- get_mask(mask_scenes, threshold = 0.4)
+      
+      #Export mask
+      name_mask <- paste0(out_path, "/", 
+                          unique_tile[i], "_", 
+                          unique_years[ii], "_", 
+                          "mask", "_",
+                          "kNDVI", ".tif")
+      
+      writeRaster(mask_layer, name_mask, names = "mask", overwrite=TRUE)
       
       ###Trend------------------------------------------------------------------
       #Period of interest
@@ -89,11 +117,27 @@ trends_sen2 <- function(root_path, mask_doy, evaluation_doy, threads) {
                          fun = fun_slope,
                          date = doy,
                          cores = threads)
+      names(trend_layer) <- "slope"
       
+      #Export slope
+      name_slope <- paste0(out_path, "/", 
+                          unique_tile[i], "_", 
+                          unique_years[ii], "_", 
+                          "slope", "_",
+                          "kNDVI", ".tif")
+        
+      writeRaster(trend_layer, name_slope, names = "slope", overwrite=TRUE)
       
+      #Release memory -just in case-
+      gc()
+      
+      } else{
+        cat(paste0("No ", unique_tile[i], " for ", unique_years[ii]))
+      }
     }
   }
 }
 
+trends_sen2(root_path, mask_doy, evaluation_doy, out_path, threads)
 
 
