789c066799f8a9ff642bdaa09ffad827f0dd8a47
J. Antonio Guzmán Q <antguz06@gmail.com>
2021-12-19 21:15:17 -0600

    sam between 0 and 1

--- src/sam.cpp
+++ src/sam.cpp
@@ -5,13 +5,14 @@ double sam(NumericVector& x, NumericVector& y) {
   
   const double conv = 180/3.141593;
   
-  double radians;
-  
   double normEM = sum(pow(y, 2));
   
-  radians = acos(sum(x * y) / sqrt(sum(pow(x,2)) * normEM));
+  double radians = acos(sum(x * y) / sqrt(sum(pow(x,2)) * normEM));
   
-  double angle = 1 - (radians * conv);
+  double angle = (radians * conv);
   
-  return angle;
+  double simiarity = (90 - angle) / 90;
+  
+  return simiarity;
+  
 }
