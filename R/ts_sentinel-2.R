cc987c85b49b710d4e480dda7b9c2fa0465de5dc
J. Antonio Guzmán Q <antguz06@gmail.com>
2022-02-10 09:06:29 -0600

    gdalcubes

--- R/ts_sentinel-2.R
+++ R/ts_sentinel-2.R
@@ -21,9 +21,17 @@ frame_files[, sensor := strsplit(files, "_")[[1]][3],
             by = seq_along(1:nrow(frame_files))]
 
 frame_files <- frame_files[sensor != "LND08",]
-frame_files <- frame_files[1:10,]
+frame_files <- frame_files[95:100,]
 
 #Creating data cubes
 SEN2 <- create_image_collection(paste0(root_path, "/", tile, "/", frame_files$files), 
-                                format = "force_L2_SEN2", 
+                                format = "/home/antonio/Documents/Github/Oak-wilt/misc/force_L2_SEN2.json", 
                                 out_file = "L8.db")
+
+SEN2
+plot(rgb=3:1, SEN2)
+
+
+raster_cube(SEN2) |>
+  select_bands(c("SWIR1")) |>
+  plot(nbreaks=10, key.pos=1)
