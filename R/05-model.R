eb9073886cbe96b1940d47e4708f1233f8591963
Antguz <antguz06@gmail.com>
2022-07-10 21:03:42 -0500

    Model implementation

--- R/05-model.R
+++ R/05-model.R
@@ -9,14 +9,13 @@ library(data.table)
 library(caret)
 library(ggplot2)
 library(viridis)
-library(mlbench)
-library(plotROC)
 library(terra)
+library(doParallel)
 
 #-------------------------------------------------------------------------------
 # Root path
 
-path <- "F:/FORCE"
+path <- "/media/antonio/antonio_ssd/FORCE"
 
 #-------------------------------------------------------------------------------
 # Reading and cleaning
@@ -41,7 +40,7 @@ data <- rbind(X0014_Y0024, X0015_Y0024)
 data <- subset(data, date == "2019")
 data$row <- 1:nrow(data)
 
-#Select minimum dCCI
+#Select pixel with the highest cover
 frame <- data[, row[which.max(weight)], by = c("ID", "tile")]
 colnames(frame)[3] <- "row"
 frame <- na.exclude(frame)
@@ -50,7 +49,10 @@ condition <- is.na(data$condition)
 data <- subset(data, condition != "")
 data <- data[, c(12, 1:11)]
 
-#Look for outlayers
+#Look for area features
+data <- subset(data, area >= (pi*3^2)) #radios higher than 3m
+
+#Look for out layers
 remove_outliers <- function(x, na.rm = TRUE, ...) {
   qnt <- quantile(x, probs=c(.25, .75), na.rm = na.rm, ...)
   H <- 1.5 * IQR(x, na.rm = na.rm)
@@ -61,18 +63,34 @@ remove_outliers <- function(x, na.rm = TRUE, ...) {
 }
 
 data[, dCCI := remove_outliers(dCCI), by = condition]
-data[, dCCI := remove_outliers(dCCI), by = condition]
 data[, dNDW := remove_outliers(dNDW), by = condition]
 data[, dCRE := remove_outliers(dCRE), by = condition]
 data[, kND := remove_outliers(kND), by = condition]
+data <- na.exclude(data)
 
-ggplot(data, aes(x= condition, y= kND, fill = condition)) +
+#dCCI
+ggplot(data, aes(x= condition, y= dCCI, fill = condition)) +
   geom_boxplot() +
   scale_fill_viridis(discrete = TRUE, alpha=0.6) +
   geom_jitter(color="black", size=0.4, alpha=0.9)
 
-#Remove NAs
-data <- na.exclude(data)
+#dNDW
+ggplot(data, aes(x= condition, y= dNDW, fill = condition)) +
+  geom_boxplot() +
+  scale_fill_viridis(discrete = TRUE, alpha=0.6) +
+  geom_jitter(color="black", size=0.4, alpha=0.9)
+
+#dCRE
+ggplot(data, aes(x= condition, y= dCRE, fill = condition)) +
+  geom_boxplot() +
+  scale_fill_viridis(discrete = TRUE, alpha=0.6) +
+  geom_jitter(color="black", size=0.4, alpha=0.9)
+
+#dkND
+ggplot(data, aes(x= condition, y= kND, fill = condition)) +
+  geom_boxplot() +
+  scale_fill_viridis(discrete = TRUE, alpha=0.6) +
+  geom_jitter(color="black", size=0.4, alpha=0.9)
 
 #Data should be clean and it is ready to export
 
@@ -86,34 +104,91 @@ fwrite(data, paste0(path, "/model/data/data_clean.csv"))
 data <- fread(paste0(path, "/model/data/data_clean.csv"))
 
 #Data split
-data[condition == "wilted", factor := "wilted", ]
-data[condition != "wilted", factor := "non_wilted", ]
+data[condition == "wilted", factor_wilted := "wilted", ]
+data[condition != "wilted", factor_wilted := "non_wilted", ]
 
-#Proportions
-table(data$factor)
+data[condition == "healthy", factor_healthy := "healthy", ]
+data[condition != "healthy", factor_healthy := "non_healthy", ]
+
+data[condition == "dead", factor_dead := "dead", ]
+data[condition != "dead", factor_dead := "non_dead", ]
+
+#Data partitions ---------------------------------------------------------------
+#Wilted ---------------------------
+table(data$factor_wilted) #622 498 
 
 #Separate
-wilted <- subset(data, factor == "wilted")
-no_wilted <- subset(data, factor == "non_wilted")
+wilted <- subset(data, factor_wilted == "wilted")
+no_wilted <- subset(data, factor_wilted == "non_wilted")
 
-#Index for training
+#Split
 ind_wilted <- createDataPartition(wilted$tile,
-                                  p = 0.5,
+                                  p = 0.5594855,
                                   list = FALSE,
                                   times = 1)
 
 ind_no_wilted <- createDataPartition(no_wilted$tile,
-                                     p = 0.75,
+                                     p = 0.5594855,
                                      list = FALSE,
                                      times = 1)
 
-#Get training and testing
-training <- rbind(wilted[ind_wilted], no_wilted[ind_no_wilted])
-testing <- rbind(wilted[!ind_wilted], no_wilted[!ind_no_wilted])
 
-#Data should be ready for the models and to export it
-fwrite(training, paste0(path, "/model/training/training.csv"))
-fwrite(testing, paste0(path, "/model/training/testing.csv"))
+#Healthy ---------------------------
+table(data$factor_healthy) #354 766
+
+#Separate
+healthy <- subset(data, factor_healthy == "healthy")
+no_healthy <- subset(data, factor_healthy == "non_healthy")
+
+#Split
+ind_healthy <- createDataPartition(healthy$tile,
+                                  p = 0.6836158,
+                                  list = FALSE,
+                                  times = 1)
+
+ind_no_healthy <- createDataPartition(no_healthy$tile,
+                                     p = 0.3159269,
+                                     list = FALSE,
+                                     times = 1)
+
+#Dead ---------------------------
+table(data$factor_dead) #268 852
+
+#Separate
+dead <- subset(data, factor_dead == "dead")
+no_dead <- subset(data, factor_dead == "non_dead")
+
+ind_dead <- createDataPartition(dead$tile,
+                                  p = 0.7014925,
+                                  list = FALSE,
+                                  times = 1)
+
+ind_no_dead <- createDataPartition(no_dead$tile,
+                                     p = 0.2206573,
+                                     list = FALSE,
+                                     times = 1)
+
+#Get training and testing export -----------------------------------------------
+#wilted
+wilted_training <- rbind(wilted[ind_wilted], no_wilted[ind_no_wilted])
+wilted_testing <- rbind(wilted[!ind_wilted], no_wilted[!ind_no_wilted])
+
+fwrite(wilted_training, paste0(path, "/model/training/wilted_training.csv"))
+fwrite(wilted_testing, paste0(path, "/model/training/wilted_testing.csv"))
+
+#Healthy
+healthy_training <- rbind(healthy[ind_healthy], no_healthy[ind_no_healthy])
+healthy_testing <- rbind(healthy[!ind_healthy], no_healthy[!ind_no_healthy])
+
+fwrite(training, paste0(path, "/model/training/healthy_training.csv"))
+fwrite(testing, paste0(path, "/model/training/healthy_testing.csv"))
+
+#Dead
+dead_training <- rbind(dead[ind_dead], no_dead[ind_no_dead])
+dead_testing <- rbind(dead[!ind_dead], no_dead[!ind_no_dead])
+
+fwrite(training, paste0(path, "/model/training/dead_training.csv"))
+fwrite(testing, paste0(path, "/model/training/dead_testing.csv"))
 
 #-------------------------------------------------------------------------------
 # Model Training
@@ -121,85 +196,149 @@ fwrite(testing, paste0(path, "/model/training/testing.csv"))
 #Repeated 10-fold cross-validation
 fitControl <- trainControl(method = "repeatedcv",
                            number = 10,
-                           repeats = 1,
+                           repeats = 10,
                            classProbs = TRUE,
                            summaryFunction = twoClassSummary,
                            savePredictions = TRUE)
 
-data_training <- training[, c(13, 9:12)]
-weights <- training$area
+#Select columns of interest
+wilted_training <- wilted_training[, c(13, 9:12)]
+healthy_training <- healthy_training[, c(14, 9:12)]
+dead_training <- dead_training[, c(15, 9:12)]
 
-#Model train
-set.seed(825)
-bayesglm_model <- train(factor ~ ., data = data_training, 
+#Model
+model_training <- function(wilted_training, healthy_training, dead_training) {
+  
+  set.seed(825)
+  wilted_model <- train(factor_wilted ~ ., data = wilted_training, 
+                          method = 'bayesglm', 
+                          trControl = fitControl,
+                          #weights = weights,
+                          metric = "ROC")
+  
+  set.seed(825)
+  healthy_model <- train(factor_healthy ~ ., data = healthy_training, 
                         method = 'bayesglm', 
                         trControl = fitControl,
+                        #weights = weights,
                         metric = "ROC")
-
-bayesglm_model <- train(factor ~ ., data = data_training, 
-                        method = 'rf', 
-                        weights = weights,
+  
+  set.seed(825)
+  dead_model <- train(factor_dead ~ ., data = dead_training, 
+                        method = 'bayesglm', 
                         trControl = fitControl,
+                        #weights = weights,
                         metric = "ROC")
+  
+  return(list(wilted = wilted_model,
+              healthy = healthy_model,
+              dead = dead_model))
+  
+}
 
+models <- model_training(wilted_training, healthy_training, dead_training)
 
-fitControl <- trainControl(method = "none",
-                           classProbs = TRUE,
-                           summaryFunction = twoClassSummary,
-                           savePredictions = TRUE)
-
-fitControl <- trainControl(method = "cv",
-                           number = 10,
-                           classProbs = TRUE,
-                           summaryFunction = twoClassSummary,
-                           savePredictions = TRUE)
-
-nnetGrid <-  expand.grid(size = seq(from = 1, to = 10, by = 1),
-                         decay = seq(from = 0.1, to = 0.5, by = 0.1))
-
-bayesglm_model <- train(factor ~ ., data = data_training, 
-                        method = 'nnet', 
-                        weights = weights,
-                        trControl = fitControl,
-                        metric = "ROC",
-                        tuneGrid = nnetGrid,
-                        allowParallel=TRUE)
-
-
-
-library(doParallel)
-no_cores <- detectCores() - 4
-cl <- makePSOCKcluster(no_cores)
-registerDoParallel(cl)
-
-bayesglm_model <- train(factor ~ ., data = data_training, 
-                        method = 'gaussprLinear', 
-                        weights = weights,
-                        trControl = fitControl,
-                        metric = "ROC",
-                        allowParallel=TRUE)
-
-stopCluster(cl)
-registerDoSEQ()
+#Export model
+saveRDS(models, "data/models/models.rds")
 
 #-------------------------------------------------------------------------------
 # Model Testing
 
-data_testing <- testing[, c(13, 9:12)]
+#Select columns of interest
+wilted_testing <- wilted_testing[, c(13, 9:12)]
+healthy_testing <- healthy_testing[, c(14, 9:12)]
+dead_testing <- dead_testing[, c(15, 9:12)]
 
-training_matrix <- data.frame(observed = as.factor(training$factor),
-                               predicted = predict(bayesglm_model))
+#Model testing
 
-testing_matrix <- data.frame(observed = as.factor(testing$factor),
-                              predicted = predict(bayesglm_model, newdata = testing))
+model_testing <- function(models, 
+                          wilted_training, wilted_testing, 
+                          healthy_training, healthy_testing, 
+                          dead_training, dead_testing) {
+  
+  #Wilted
+  wilted_training_m <- data.frame(observed = as.factor(wilted_training$factor_wilted),
+                                  predicted = predict(models$wilted))
+  
+  wilted_testing_m <- data.frame(observed = as.factor(wilted_testing$factor_wilted),
+                                 predicted = predict(models$wilted, newdata = wilted_testing))
+  
+  wilted_training_cm <- caret::confusionMatrix(wilted_training_m$observed, wilted_training_m$predicted)
+  wilted_testing_cm <- caret::confusionMatrix(wilted_testing_m$observed, wilted_testing_m$predicted)
+  
+  #Healthy
+  healthy_training_m <- data.frame(observed = as.factor(healthy_training$factor_healthy),
+                                  predicted = predict(models$healthy))
+  
+  healthy_testing_m <- data.frame(observed = as.factor(healthy_testing$factor_healthy),
+                                 predicted = predict(models$healthy, newdata = healthy_testing))
+  
+  healthy_training_cm <- caret::confusionMatrix(healthy_training_m$observed, healthy_training_m$predicted)
+  healthy_testing_cm <- caret::confusionMatrix(healthy_testing_m$observed, healthy_testing_m$predicted)
+  
+  #Dead
+  dead_training_m <- data.frame(observed = as.factor(dead_training$factor_dead),
+                                predicted = predict(models$dead))
+  
+  dead_testing_m <- data.frame(observed = as.factor(dead_testing$factor_dead),
+                               predicted = predict(models$dead, newdata = dead_testing))
+  
+  dead_training_cm <- caret::confusionMatrix(dead_training_m$observed, dead_training_m$predicted)
+  dead_testing_cm <- caret::confusionMatrix(dead_testing_m$observed, dead_testing_m$predicted)
+  
+  #Merge
+  wilted_training <- cbind(data.table(Condition = "Wilted", Step = "Training"), 
+                           matrix(wilted_training_cm$overall, nrow = 1), 
+                           matrix(wilted_training_cm$byClass, nrow = 1))
+  
+  wilted_testing <- cbind(data.table(Condition = "Wilted", Step = "Testing"), 
+                          matrix(wilted_testing_cm$overall, nrow = 1), 
+                          matrix(wilted_testing_cm$byClass, nrow = 1))
+  
+  healthy_training <- cbind(data.table(Condition = "Healthy", Step = "Training"), 
+                           matrix(healthy_training_cm$overall, nrow = 1), 
+                           matrix(healthy_training_cm$byClass, nrow = 1))
+  
+  healthy_testing <- cbind(data.table(Condition = "Healthy", Step = "Testing"), 
+                          matrix(healthy_testing_cm$overall, nrow = 1), 
+                          matrix(healthy_testing_cm$byClass, nrow = 1))
+  
+  dead_training <- cbind(data.table(Condition = "Dead", Step = "Training"), 
+                           matrix(dead_training_cm$overall, nrow = 1), 
+                           matrix(dead_training_cm$byClass, nrow = 1))
+  
+  dead_testing <- cbind(data.table(Condition = "Dead", Step = "Testing"), 
+                          matrix(dead_testing_cm$overall, nrow = 1), 
+                          matrix(dead_testing_cm$byClass, nrow = 1))
+  
+  frame <- rbind(wilted_training, wilted_testing,
+                 healthy_training, healthy_testing,
+                 dead_training, dead_testing)
+  
+  colnames(frame)[3:9] <- names(dead_training_cm$overall)
+  colnames(frame)[10:20] <- names(dead_training_cm$byClass)
+  
+  return(frame)
+  
+}
 
-training_cm <- caret::confusionMatrix(training_matrix$observed, training_matrix$predicted)
-testing_cm <- caret::confusionMatrix(testing_matrix$observed, testing_matrix$predicted)
+validation <- model_testing(models, 
+                            wilted_training, wilted_testing, 
+                            healthy_training, healthy_testing, 
+                            dead_training, dead_testing)
 
-ggplot(bayesglm_model$pred, aes(m=wilted, d=factor(obs, levels = c("wilted", "non_wilted")))) + 
-  geom_roc() + 
-  coord_equal() +
-  style_roc()
+
+
+
+
+
+
+
+
+
+
+
+
 
 prob <- predict(bayesglm_model, type = "prob")[, 2]
 plot(prob ~ data_training$dCCI)
@@ -235,6 +374,3 @@ probability <- mask(probability, scene$kND, maskvalues = 0)
 
 
 writeRaster(probability, paste0(path, "/level4/X0015_Y0024/2019_prob2.tif"), overwrite=TRUE)
-
-
-avNNet(x, ...)
