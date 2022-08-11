abf937eb5c01d105a1d8e76c4f49b18aaae87408
Antonio Guzman <antguz06@gmail.com>
2022-08-10 22:27:06 -0500

    cleaning

--- R/04-data_cleaning.R
+++ R/04-data_cleaning.R
@@ -14,13 +14,15 @@ library(terra)
 #-------------------------------------------------------------------------------
 #Arguments
 
-#' @param root_path: select the root path of where the .txt is located
-#' @param out_path: path and name of the .txt outputs
+#' @param root_path select the root path of where the .txt is located
+#' @param out_path path and name of the .txt outputs
+#' @param select_method Method to subset
 
 path <- "/media/antonio/antonio_ssd/FORCE"
 
-root_path <- paste0(path, "/model/data/X0015_0024_dkVI.txt")
-out_path <- paste0(path, "/model/data/X0015_0024_dkVI_clean.txt")
+root_path <- paste0(path, "/level3_shifted/X0014_0024_VI.txt")
+out_path <- paste0(path, "/level3_shifted/X0014_0024_VI_clean.txt")
+select_method <- "TSI"
 
 data_clean(root_path, out_path)
 
@@ -31,18 +33,21 @@ data_clean <- function(root_path, out_path) {
   
   #Read and add row number
   data <- fread(root_path)
+  data <- subset(data, method == select_method)
+  
+  #dcast
+  data <- dcast(data, ID + condition + date + area + x + y + weight ~ VI, value.var = "value")
   data$row <- 1:nrow(data)
   
   #Get pixel with the high weight
-  unique_id <- unique(data$ID)
   frame <- data[, row[which.max(weight)], by = c("ID", "date")]
   colnames(frame)[3] <- "row"
   frame <- na.exclude(frame)
   
   #Select rows
-  data <- data[frame$row, ]
+  data <- data[frame$row, .SD, .SDcols = c(1:11)]
   
   #Export
   fwrite(data, out_path, sep = "\t")
-
+  
 }
